a6608a6985977b34bbb986eb8e171c98e935a2bb
Nikita Ponomarev <nikita.ponomarev@intel.com>
2019-04-17 16:58:24 +0300

    Vim undo fix

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 # Install gdbinit
 wget -P ~ git.io/.gdbinit
 
+# Vim undo folder
+mkdir .vim/undo
 
 # Install/Update  Vim-Plug
-# curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 # Install universal ctags
 # git clone https://github.com/universal-ctags/ctags.git
 # cd ctags
